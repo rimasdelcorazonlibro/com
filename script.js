78d7ee82a8a864485b36af22ff7a9ebede573e71
rimasdelcorazonlibro <160087616+rimasdelcorazonlibro@users.noreply.github.com>
2024-02-14 22:15:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('leerLibroBtn').addEventListener('click', function() {
+    // Abre una nueva ventana para la lectura del PDF (Este código abrirá una ventana vacía, debes reemplazarlo con la lógica de tu aplicación).
+    window.open('Libro Emi.pdf', '_blank', 'width=800,height=600');
+});
